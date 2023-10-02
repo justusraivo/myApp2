0441a3bfa78be7ee14267df45664df9c40ad04af
justusraivo <82445027+justusraivo@users.noreply.github.com>
2023-10-02 19:21:34 +0300

    First commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,4 +5,12 @@ export const routes: Routes = [
     path: '',
     loadChildren: () => import('./tabs/tabs.routes').then((m) => m.routes),
   },
+  {
+    path: 'about',
+    loadComponent: () => import('./about/about.page').then( m => m.AboutPage)
+  },
+  {
+    path: 'test',
+    loadComponent: () => import('./test/test.page').then( m => m.TestPage)
+  },
 ];
---
+++ src/app/test/test.page.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { IonicModule } from '@ionic/angular';
+
+@Component({
+  selector: 'app-test',
+  templateUrl: './test.page.html',
+  styleUrls: ['./test.page.scss'],
+  standalone: true,
+  imports: [IonicModule, CommonModule, FormsModule]
+})
+export class TestPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
